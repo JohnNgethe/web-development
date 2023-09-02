9eb22ed5e7adc0171605c3f6338b8cb68e7e84fe
JohnNgethe <Jngethe07@gmail.com>
2023-09-02 15:36:18 +0300

    ch10 added a compose page and eddited with bootstrap

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -31,7 +31,11 @@ app.get("/contact", (req, res)=>{
 
 app.get("/compose", (req,res)=>{
   res.render("compose");
-})
+});
+
+app.post("/compose", (req, res)=>{
+  console.log(req.body.postTitle);
+} );
 
 
 
