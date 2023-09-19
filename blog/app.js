594e29c797500c2b12d767950f8f4b69f7c58a21
JohnNgethe <Jngethe07@gmail.com>
2023-09-19 13:44:15 +0300

    Adding DB to blog site

--- blog/app.js
+++ blog/app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -19,6 +20,22 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+
+mongoose.connect("mongodb://127.0.0.1:27017/blogDB", {
+  useNewUrlParser: true,
+});
+
+const postSchema = {
+  title: {
+    type: String
+  },
+  content: {
+    type: String
+  }
+};
+
+
+const Post = mongoose.model("Post", postSchema);
 let posts = [];
 
 app.get("/", (req, res) => {
@@ -41,11 +58,11 @@ app.get("/compose", (req, res) => {
 });
 
 app.post("/compose", (req, res) => {
-  const post = {
+  const post = new Post({
     title: req.body.postTitle,
-    content: req.body.postBody,
-  };
-  posts.push(post);
+    content: req.body.postBody
+  });
+  post.save();
   res.redirect("/");
 });
 
