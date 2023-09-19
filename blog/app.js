58a11e43ed6d0e8f58608c79a35319a347b64df8
JohnNgethe <Jngethe07@gmail.com>
2023-09-19 14:50:29 +0300

    Successfully Connected DB to Simple Web Page

--- blog/app.js
+++ blog/app.js
@@ -6,8 +6,6 @@ const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
 
-const homeStartingContent =
-  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
@@ -36,13 +34,17 @@ const postSchema = {
 
 
 const Post = mongoose.model("Post", postSchema);
-let posts = [];
 
-app.get("/", (req, res) => {
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts,
-  });
+app.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.render("home", {
+            posts: posts,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("An error occurred while fetching posts.");
+  }
 });
 
 app.get("/about", (req, res) => {
@@ -66,22 +68,27 @@ app.post("/compose", (req, res) => {
   res.redirect("/");
 });
 
-app.get("/posts/:postName", (req, res) => {
-  const requestedTitle = _.lowerCase(req.params.postName);
+app.get("/posts/:postId", async (req, res) => {
+  const requestedPostId = req.params.postId;
 
-  // to loop through the array and store the title to the variable below
-  posts.forEach((post) => {
-    const storedTitle = _.lowerCase(post.title);
+  try {
+    const post = await Post.findOne({ _id: requestedPostId });
 
-    if (storedTitle === requestedTitle) {
+    if (post) {
       res.render("post", {
-        unoPost: post,
+        title: post.title,
+        content: post.content,
       });
     } else {
-      console.log("missing");
+      console.log("Post not found");
+      res.status(404).send("Post not found.");
     }
-  });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("An error occurred while fetching the post.");
+  }
 });
+
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
