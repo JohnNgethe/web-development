013fbf10c4175a32b52d50121bd293adb315c141
JohnNgethe <Jngethe07@gmail.com>
2023-10-15 22:41:29 +0300

    chained route handlers

--- wiki-API/app.js
+++ wiki-API/app.js
@@ -12,10 +12,17 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+app.use(express.static("public"));
+
 mongoose.connect(process.env.DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+//check mongo connection
+const db = mongoose.connection;
+db.on("error", (error) => console.error("MongoDB connection error:", error));
+db.once("open", () => console.log("Connected to MongoDB"));
 
 const articleSchema = {
     title: {
@@ -28,8 +35,52 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
+app.route("/articles")
+  .get(async (req, res) => {
+    try {
+      const foundArticles = await Article.find();
+      res.send(foundArticles);
+    } catch (err) {
+      console.log(err);
+      res.send(err);
+    }
+  }) 
+  .post(async (req, res) => {
+    try {
+      console.log(req.body.title);
+      console.log(req.body.content);
 
-app.use(express.static("public"));
+      const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content,
+      });
+      newArticle
+        .save()
+        .then(() => {
+          res.send("Successfully Added");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } catch (error) {
+      console.log(err);
+    }
+  })
+  .delete(async (req, res) => {
+    try {
+      Article.deleteMany()
+        .then(() => {
+          console.log("Deleted all articles");
+          res.send("Deleted all articles");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } catch (error) {
+      console.log(err);
+    }
+  });
+
 
 
 let port = process.env.PORT || 3000;
