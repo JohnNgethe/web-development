9f7dde149260182654c4e411f5ee6f27300e2601
JohnNgethe <Jngethe07@gmail.com>
2023-10-23 23:53:12 +0300

    added node packages

--- Secrets/app.js
+++ Secrets/app.js
@@ -1,1 +1,43 @@
 //jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+require("dotenv").config();
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static("public"));
+
+
+app.get("/", (req, res)=>{
+    res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+
+
+
+
+
+
+let port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server started successfully on port ${port}`);
+});
+
+
